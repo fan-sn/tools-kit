f27f7e1845d17d6c15f0f03ba69ac204b1da8d03
fan-sn <48694305+fan-sn@users.noreply.github.com>
2025-07-15 15:17:44 +0800

    Update le.hpp

--- le.hpp
+++ le.hpp
@@ -5,12 +5,12 @@ static std::vector<std::vector<double>> tsp=sampleTSP();
 static std::vector<std::vector<std::vector<double>>> tsps(4, tsp);
 static aiCompute e=aiCompute();
 double upd(const std::vector<std::vector<std::vector<double>>> &v3d) {
-  for (auto dist:v3d) {
-    std::vector<int> aco_path = e.antColonyOptimizeTSP(dist, 20, 100);
-    d+=length(dist);
-  }
+    for (auto dist:v3d) {
+        d+=length(dist);
+    }
 }
 double length(const std::vector<std::vector<double>> &dist){
+    std::vector<int> aco_path = e.antColonyOptimizeTSP(dist, 20, 100);
     for (size_t i = 0; i < dist.size(); ++i) {
         int from = aco_path[i];
         int to = aco_path[(i + 1) % aco_path.size()];
